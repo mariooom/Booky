2b9ba240412d2e73aad124a45cec8cd2024ec74a
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2025-09-16 14:13:24 +0300

    add sliding animation to spalsh view

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -2,8 +2,34 @@ import 'package:bookly/constants.dart';
 import 'package:bookly/core/utilities/assests_data.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late Animation<Offset> slidingAnimation;
+  late AnimationController animationController;
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: Offset(0, 10),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,13 +40,32 @@ class SplashViewBody extends StatelessWidget {
         children: [
           Image.asset(AssestsData.logo),
           const SizedBox(height: 15),
-          Text(
+          SlidingText(slidingAnimation: slidingAnimation),
+        ],
+      ),
+    );
+  }
+}
+
+class SlidingText extends StatelessWidget {
+  const SlidingText({super.key, required this.slidingAnimation});
+
+  final Animation<Offset> slidingAnimation;
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: slidingAnimation,
+      builder: (context, _) {
+        return SlideTransition(
+          position: slidingAnimation,
+          child: Text(
             textAlign: TextAlign.center,
             'Read Free Books',
             style: TextStyle(fontSize: 22, fontFamily: kSectraFine),
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
