bda5d0c02fcb21a289bea6b574e99754d7226db7
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2025-09-16 14:45:10 +0300

    create custom appbar

--- lib/features/home/presentaion/views/home_view.dart
+++ lib/features/home/presentaion/views/home_view.dart
@@ -1,9 +1,10 @@
+import 'package:bookly/features/home/presentaion/views/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: HomeViewBody());
   }
 }
---
+++ lib/features/home/presentaion/views/widgets/home_view_body.dart
@@ -0,0 +1,11 @@
+import 'package:bookly/features/home/presentaion/views/widgets/custom_appbar.dart';
+import 'package:flutter/material.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [CustomAppbar()]);
+  }
+}
