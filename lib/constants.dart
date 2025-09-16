7f48d3365d0f90e613457c26813750138736517f
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2025-09-16 14:32:31 +0300

    refactor initState and navigagteHome

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,4 @@ import 'package:flutter/material.dart';
 
 const kWallPaper = Color(0xff100B20);
 const kSectraFine = 'GT Sectra Fine';
+const kDuration = Duration(milliseconds: 300);
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,6 +1,10 @@
+import 'package:bookly/constants.dart';
 import 'package:bookly/core/utilities/assests_data.dart';
+import 'package:bookly/features/home/presentaion/views/home_view.dart';
 import 'package:bookly/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -13,30 +17,8 @@ class _SplashViewBodyState extends State<SplashViewBody>
     with SingleTickerProviderStateMixin {
   late Animation<Offset> slidingAnimation;
   late AnimationController animationController;
-  @override
-  void initState() {
-    super.initState();
-    initSlidingAnimation();
-  }
-
-  void initSlidingAnimation() {
-      animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 3),
-    );
-    slidingAnimation = Tween<Offset>(
-      begin: Offset(0, 10),
-      end: Offset.zero,
-    ).animate(animationController);
-    animationController.forward();
-  }
 
   @override
-  void dispose() {
-    super.dispose();
-    animationController.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -51,5 +33,38 @@ class _SplashViewBodyState extends State<SplashViewBody>
       ),
     );
   }
-}
 
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+    navigateToHome();
+  }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+    slidingAnimation = Tween<Offset>(
+      begin: Offset(0, 10),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(
+        () => HomeView(),
+        transition: Transition.fadeIn,
+        duration: kDuration,
+      );
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+}
