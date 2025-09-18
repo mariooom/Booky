e0067e676644c2cd9bb69cc40dbddf758b824ba0
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2025-09-18 12:27:00 +0300

    updates

--- lib/features/search/presentation/views/widgets/custom_search_field.dart
+++ lib/features/search/presentation/views/widgets/custom_search_field.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomSearchField extends StatelessWidget {
   const CustomSearchField({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15.0),
-      child: TextField(
-        decoration: InputDecoration(
-          enabledBorder: buildOutlinedInputBorder(),
-          focusedBorder: buildOutlinedInputBorder(),
-          hintText: 'Search',
-          suffixIcon: IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.search,
-              color: const Color.fromARGB(255, 220, 99, 62),
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () {
+            GoRouter.of(context).pop();
+          },
+          icon: Icon(Icons.close, color: Colors.white),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.74,
+          child: TextField(
+            decoration: InputDecoration(
+              enabledBorder: buildOutlinedInputBorder(),
+              focusedBorder: buildOutlinedInputBorder(),
+              hintText: 'Search',
+              suffixIcon: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.search,
+                  color: const Color.fromARGB(255, 220, 99, 62),
+                ),
+              ),
             ),
+            cursorColor: const Color.fromARGB(255, 220, 99, 62),
           ),
         ),
-        cursorColor: const Color.fromARGB(255, 220, 99, 62),
-      ),
+      ],
     );
   }
 
