ba145f6d4135c764da514bcdf3f13464e14deabc
Mariam Hegab <79330137+mariooom@users.noreply.github.com>
2025-09-18 12:27:51 +0300

    updates

---
+++ lib/features/search/presentation/views/search_view_body.dart
@@ -0,0 +1,28 @@
+import 'package:bookly/core/utilities/styles.dart';
+import 'package:bookly/features/search/presentation/views/widgets/custom_search_field.dart';
+import 'package:bookly/features/search/presentation/views/widgets/results.dart';
+import 'package:flutter/material.dart';
+
+class SearchViewBody extends StatelessWidget {
+  const SearchViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: Column(
+        children: [
+          //SearchAppbar(),
+          CustomSearchField(),
+          SizedBox(height: 5),
+          SizedBox(
+            height: 25,
+            width: MediaQuery.of(context).size.width * 0.85,
+            child: Text('Search Results', style: Styles.textStyle16),
+          ),
+          Expanded(child: SearchResults()),
+        ],
+      ),
+    );
+  }
+}
